197515a7e623d0af7bdba5333ff59014ed72fc7c
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2022-07-02 17:18:41 +0400

    Create eosc.sh

---
+++ eosc.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Enable NOPASSWD Sudo
+su -c "echo \"$USER ALL=(ALL:ALL) NOPASSWD: ALL\" | EDITOR=\"tee -a\" visudo"
+
+# Make screen brightness changeable
+sudo chmod a+rw /sys/class/backlight/intel_backlight/brightness
+
+# Install packages
+sudo pacman -S lightdm-webkit2-greeter light zsh flameshot kitty vim exa bat wmctrl
+
+# Install LightDM Aether theme
+git clone https://github.com/NoiSek/Aether.git
+sudo cp --recursive Aether /usr/share/lightdm-webkit/themes/
+rm -rf Aether
+sudo sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = lightdm-webkit-theme-aether #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+sudo ln -s /usr/share/lightdm-webkit/themes/Aether /usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether
+sudo sed -i "s/greeter-session = lightdm-slick-greeter/greeter-session = lightdm-webkit2-greeter/g" /etc/lightdm/lightdm.conf
+sudo cp /usr/share/lightdm-webkit/themes/Aether/src/img/default-user.png /var/lib/AccountsService/icons/$USER
+sudo sed -i "s/Icon=\/home\/$USER\/.face/Icon=\/var\/lib\/AccountsService\/icons\/$USER/g" /var/lib/AccountsService/users/$USER
+
+# Change default shell to ZSH
+sudo chsh -s /bin/zsh $USER
+
+# Move dotfiles to home directory
+mv $HOME/eos-customizer/dotfiles $HOME/
+
+# Configure BSPWM & SXHKD
+rm -rf $HOME/.config/bspwm
+rm -rf $HOME/.config/sxhkd
+mv $HOME/dotfiles/bspwm $HOME/.config/
+mv $HOME/dotfiles/sxhkd $HOME/.config/
+
+mv $HOME/dotfiles/wallpapers $HOME/.config/
+
+sudo find $HOME/.config/bspwm -type f -exec chmod +x {} \;
+sudo find $HOME/.config/sxhkd -type f -exec chmod +x {} \;
+
+# Configure ZSH
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/zsh-syntax-highlighting
+mv $HOME/dotfiles/.zshrc $HOME/
+
+# Configure Bash
+mv $HOME/dotfiles/.bashrc $HOME/
+
+# Configure VIM
+curl -fLso ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mv $HOME/dotfiles/.vimrc $HOME/
+
+# Configure Rofi
+mv $HOME/dotfiles/rofi $HOME/.config/
+
+# Configure Kitty
+mv $HOME/dotfiles/kitty $HOME/.config/
+
+# Configure Picom 
+mv $HOME/dotfiles/picom $HOME/.config/
+
+# Configure Polybar
+mv $HOME/dotfiles/polybar $HOME/.config/
+
+for script in $HOME/.config/polybar/scripts/*; do
+    sudo chmod +x $script
+done
+
+# Install Arabic font
+unzip $HOME/eos-customizer/khebrat-musamim.zip
+rm $HOME/eos-customizer/khebrat-musamim.zip
+sudo mkdir -p /usr/share/fonts/TTF
+sudo mv "$HOME/eos-customizer/18 Khebrat Musamim Regular.ttf" /usr/share/fonts/TTF/
+
+sudo mv $HOME/dotfiles/fonts.conf /etc/fonts/
+sudo cp /etc/fonts/fonts.conf /etc/fonts/local.conf
+
+# Install GRUB theme
+sudo mkdir -p /boot/grub/themes
+sudo mkdir /boot/grub/themes/endeavourOS
+sudo mv endeavourOS.tar /boot/grub/themes/endeavourOS/
+sudo tar xf /boot/grub/themes/endeavourOS/endeavourOS.tar -C /boot/grub/themes/endeavourOS/
+sudo rm /boot/grub/themes/endeavourOS/endeavourOS.tar
+sudo sed -i "s/GRUB_GFXMODE=auto/GRUB_GFXMODE=1920x1080/g" /etc/default/grub
+sudo sed -i "s/#GRUB_THEME=.*/GRUB_THEME=\"\/boot\/grub\/themes\/endeavourOS\/theme.txt\"/g" /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# Install cursor
+tar -xf $HOME/eos-customizer/macOSBigSur.tar.gz
+sudo mv $HOME/eos-customizer/macOSBigSur /usr/share/icons/
+rm -rf $HOME/eos-customizer/macOSBigSur.tar.gz
+
+sudo sed -i "s/Inherits=Adwaita/Inherits=macOSBigSur/g" /usr/share/icons/default/index.theme
+
+# Remove script directory
+rm -rf $HOME/eos-customizer
